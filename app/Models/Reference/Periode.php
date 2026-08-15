83c55e72d7c2912504952ccc2fe2c31378c69a92
esekan <r4gamia@gmail.com>
2026-08-15 19:00:28 +0000

    refactor: Simplifier le modèle Periode en retirant les traits inutilisés et en corrigeant les noms des attributs

--- app/Models/Reference/Periode.php
+++ app/Models/Reference/Periode.php
@@ -2,22 +2,20 @@
 
 namespace App\Models\Reference;
 
-use App\Domain\Shared\Traits\HasPublicUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Periode extends Model
 {
-    use HasFactory, HasPublicUuid, SoftDeletes;
+    use HasFactory;
 
     protected $table = 'periodes';
 
     protected $guarded = [];
 
     protected $casts = [
-        'debut_le' => 'date',
-        'fin_le' => 'date',
+        'date_debut' => 'date',
+        'date_fin' => 'date',
         'cloture_le' => 'datetime',
         'actif' => 'boolean',
     ];
